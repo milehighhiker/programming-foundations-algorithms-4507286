e92a6950ba2f01cfe184eeb8582afa98d698d01b
milehighhiker <142116138+milehighhiker@users.noreply.github.com>
2025-04-24 17:31:11 -0500

    Update Anagram.py

--- Finished/PracticalExamples/Anagram.py
+++ Finished/PracticalExamples/Anagram.py
@@ -1,24 +1,25 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+print(isAnagram("racecar", "carrace"))
+
+def isAnagram(self, s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+    
+    s_map = dict()
+    for c in s:
+        if c in s_map.keys():
+            s_map[c] += 1
+        else:
+            s_map[c] = 1
+    
+    t_map = dict()
+    for c in t:
+        if c in t_map.keys():
+            t_map[c] += 1
+        else:
+            t_map[c] = 1
+
+    for v1, v2 in s_map.values(), t_map.values():
+        if v1 != v2:
             return False
-        
-        s_map = dict()
-        for c in s:
-            if c in s_map.keys():
-                s_map[c] += 1
-            else:
-                s_map[c] = 1
-        
-        t_map = dict()
-        for c in t:
-            if c in t_map.keys():
-                t_map[c] += 1
-            else:
-                t_map[c] = 1
 
-        for v1, v2 in s_map.values(), t_map.values():
-            if v1 != v2:
-                return False
-
-        return True
+    return True
